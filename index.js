fd5541754d830febf0ee5e16e5b17b4e6bb907d8
vesa haaparanta <vesahaaparanta@hotmail.fi>
2022-04-03 21:45:17 +0300

    3.4

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.get('/api/persons/:id', (request, response) => {
     }
 })
 
-app.delete('/api/notes/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     notes = persons.filter(note => note.id !== id)
     response.status(204).end()
